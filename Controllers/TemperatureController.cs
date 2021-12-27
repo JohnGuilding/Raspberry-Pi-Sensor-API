6070ff8623652ae85c4b48c0f84dcc78be2a62e9
John Guilding <jwguilding@gmail.com>
2021-12-27 19:57:04 +0000

    Rename controller and class to better suit domain

---
+++ Controllers/TemperatureController.cs
@@ -0,0 +1,22 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Raspberry_Pi_Sensor_API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TemperatureController : ControllerBase
+    {
+        private readonly ILogger<TemperatureController> _logger;
+
+        public TemperatureController(ILogger<TemperatureController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet(Name = "GetTemperatureRecording")]
+        public TemperatureRecording GetTemperature()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- TemperatureRecording.cs
+++ TemperatureRecording.cs
@@ -1,13 +1,11 @@
 namespace Raspberry_Pi_Sensor_API
 {
-    public class WeatherForecast
+    public class TemperatureRecording
     {
         public DateTime Date { get; set; }
 
         public int TemperatureC { get; set; }
 
         public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-
-        public string? Summary { get; set; }
     }
 }
