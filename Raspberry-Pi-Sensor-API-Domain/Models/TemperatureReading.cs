5f1697d8c088e5f85087ef5b6ce85bbaff74ea26
John Guilding <jwguilding@gmail.com>
2022-02-18 17:09:08 +0000

    Fix project file paths

---
+++ Raspberry-Pi-Sensor-API-Domain/Models/TemperatureReading.cs
@@ -0,0 +1,11 @@
+﻿namespace Raspberry_Pi_Sensor_API.Domain.Models
+{
+    public class TemperatureReading
+    {
+        public DateTime Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+    }
+}
---
+++ Raspberry-Pi-Sensor-API-Infrastructure/Repositories/TemperatureRepository.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.Extensions.Options;
+using MongoDB.Driver;
+using Raspberry_Pi_Sensor_API.Domain.Models;
+using Raspberry_Pi_Sensor_API.Repositories.Models;
+
+namespace Raspberry_Pi_Sensor_API.Repositories
+{
+    public class TemperatureRepository : ITemperatureRepository
+    {
+        private readonly IMongoCollection<TemperatureReadingEntity> temperatureCollection;
+
+        public TemperatureRepository(
+            IOptions<SensorMeasurementsDatabaseSettings> sensorMeasurementDatabaseSettings)
+        {
+            var mongoClient = new MongoClient(
+                sensorMeasurementDatabaseSettings.Value.ConnectionString);
+
+            var mongoDatabase = mongoClient.GetDatabase(
+                sensorMeasurementDatabaseSettings.Value.DatabaseName);
+
+            temperatureCollection = mongoDatabase.GetCollection<TemperatureReadingEntity>(
+                sensorMeasurementDatabaseSettings.Value.TemperatureCollectionName);
+        }
+
+        public async Task<List<TemperatureReading>> GetTemperatureReadings()
+        {
+            var temperatureReadingEntities = await temperatureCollection.FindAsync(_ => true);
+
+            var temperatureReadings = temperatureReadingEntities.ToList()
+                .Select(entity => new TemperatureReading()
+                {
+                    Date = entity.ReadingDate,
+                    TemperatureC = entity.TemperatureC,
+                })
+                .ToList();
+
+
+            return temperatureReadings;
+        }
+
+        public async Task<TemperatureReading> SendTemperatureReading(TemperatureReading temperatureReading)
+        {
+            var newTemperatureReading = new TemperatureReadingEntity()
+            {
+                ReadingDate = temperatureReading.Date,
+                TemperatureC = temperatureReading.TemperatureC,
+                TemperatureF = temperatureReading.TemperatureF,
+            };
+
+            await temperatureCollection.InsertOneAsync(newTemperatureReading);
+
+            return temperatureReading;
+        }
+    }
+}
